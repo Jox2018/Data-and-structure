66cb7f9ed2544023c6221eb735e00f667f05773c
Milk <q1149088412@163.com>
2020-06-14 11:00:09 +0800

    更新了手写快速排序

--- Mycode/src/com/Algorithm/sort/quickSort.java
+++ Mycode/src/com/Algorithm/sort/quickSort.java
@@ -1,18 +1,40 @@
 package com.Algorithm.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * 快速排序
  */
 public class quickSort {
     public static void main(String[] args) {
-        int[] arr = {-9, 78, 0, 23, -567, 70};
+        /*int[] arr = {-9, 78, 0, 23, -567, 70};
         System.out.println("原始数组：");
         System.out.println(Arrays.toString(arr));
         quickSort(arr, 0, arr.length - 1);
         System.out.println("排序后的数组：");
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));*/
+
+        int[] arr = new int[8000000];
+        for (int i = 0; i < 8000000; i++) {
+            arr[i] = (int) (Math.random() * 8000000);//生成一个[0-8000000)数
+        }
+
+        Date date1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(date1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+
+        quickSort(arr,0,arr.length - 1);
+
+        //System.out.println("排序后");
+        //System.out.println(Arrays.toString(arr));
+        Date date2 = new Date();
+        String date2Str = simpleDateFormat.format(date2);
+        System.out.println("排序后的时间是=" + date2Str);
+        //System.out.println(Arrays.toString(arr));
     }
 
     public static void quickSort(int[] arr, int left, int right) {
